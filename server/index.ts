70fa702362746546cea425d6ba13a9ecaf94e2ee
GeoAstronaute <jeanmarc.royy@gmail.com>
2018-11-01 19:43:40 +0100

    (Server) index.ts: fix permission error

--- server/index.ts
+++ server/index.ts
@@ -44,29 +44,28 @@ export default class FirebaseAuthentication {
         this.app.api.permissions.add({
             name: 'Firebase Connected User ID',
             description: 'Inject Firebase User uid',
-            middleware:
-`(req, res, next) => {
-    // Authorization: Bearer
-    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        // Handle token presented as a Bearer token in the Authorization header
-        const token = req.headers.authorization.split(' ')[1];
-        admin.auth().verifyIdToken(token)
-            .then((decodedToken) => {
-                const uid = decodedToken.uid;
-                if (req.method === 'get' || req.method === 'delete') {
-                    req.query.id_user = uid;
+            middleware: (req, res, next) => {
+                // Authorization: Bearer
+                if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+                    // Handle token presented as a Bearer token in the Authorization header
+                    const token = req.headers.authorization.split(' ')[1];
+                    admin.auth().verifyIdToken(token)
+                        .then((decodedToken) => {
+                            const uid = decodedToken.uid;
+                            if (req.method === 'get' || req.method === 'delete') {
+                                req.query.id_user = uid;
+                            } else {
+                                req.body.id_user = uid;
+                            }
+                            return next();
+                        }).catch(() => {
+                            // Handle error
+                            res.status(401).send(new Error('Unauthorized'));
+                        });
                 } else {
-                    req.body.id_user = uid;
+                    res.status(401).send(new Error('Unauthorized'));
                 }
-                return next();
-            }).catch(() => {
-                // Handle error
-                res.status(401).send(new Error('Unauthorized'));
-            });
-    } else {
-        res.status(401).send(new Error('Unauthorized'));
-    }
-}`,
+            },
             readOnly: true
         });
     }
