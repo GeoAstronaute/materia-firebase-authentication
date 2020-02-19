8e7cb5f1065501df49eac9a691551704f6d2a904
GeoAstronaute <jeanmarc.royy@gmail.com>
2020-02-19 13:07:08 +0100

    deps: upgrade firebase-admin to v8

--- client/public_api.ts
+++ client/public_api.ts
@@ -1,4 +1,5 @@
 export * from './components/firebase-authentication/firebase-authentication.component';
+export * from './dialogs/firebase-authentication-setup/firebase-authentication-setup.component';
 export * from './firebase-authentication.module';
 export * from './models/firebase-user.model';
 export * from './models/firebase-authentication-settings.model';
